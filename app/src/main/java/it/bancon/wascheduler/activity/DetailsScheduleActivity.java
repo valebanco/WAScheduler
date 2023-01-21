3f0beb78b8d47254309fdeb50e31ffe5d06d5864
valebanco <bancignano@gmail.com>
2023-01-21 21:04:20 +0100

    - Creata base per la gestione della rimozione e della modifica di una schedulazione

--- app/src/main/java/it/bancon/wascheduler/activity/DetailsScheduleActivity.java
+++ app/src/main/java/it/bancon/wascheduler/activity/DetailsScheduleActivity.java
@@ -5,6 +5,11 @@ import it.bancon.wascheduler.R;
 import it.bancon.wascheduler.configuration.AppContractClass;
 import it.bancon.wascheduler.model.ContactModel;
 import it.bancon.wascheduler.model.SchedulationDetails;
+import it.bancon.wascheduler.utils.IOUtils;
+import it.bancon.wascheduler.validator.ScheduleValidatorForm;
+import it.bancon.wascheduler.view.CompletedFragment;
+import it.bancon.wascheduler.view.RemoveScheduleConfirmationDialogFragment;
+import it.bancon.wascheduler.view.UpdateScheduleConfirmationDialogFragment;
 
 import android.os.Bundle;
 import android.os.Parcel;
@@ -12,7 +17,13 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class DetailsScheduleActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DetailsScheduleActivity extends AppCompatActivity
+        implements UpdateScheduleConfirmationDialogFragment.UpdateScheduleListener,
+        RemoveScheduleConfirmationDialogFragment.RemoveScheduleListener {
     SchedulationDetails schedulationDetailsSelected;
     private TextView textTimeSelected;
     private TextView textDateSelected;
@@ -21,6 +32,10 @@ public class DetailsScheduleActivity extends AppCompatActivity {
     private EditText editTextDescription;
     private EditText editTextContact;
     private EditText editTextMessage;
+
+    private List<ContactModel> contacts;
+    private UpdateScheduleConfirmationDialogFragment updateFragment;
+    private RemoveScheduleConfirmationDialogFragment removeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +48,8 @@ public class DetailsScheduleActivity extends AppCompatActivity {
         editTextMessage = findViewById(R.id.editTextMessage);
         textDateSelected = findViewById(R.id.textViewShowDateSelected);
         textTimeSelected = findViewById(R.id.textViewShowTimeSelected);
+
+        contacts = new ArrayList<>();
 
         schedulationDetailsSelected  = getIntent().getParcelableExtra(AppContractClass.KEY_PARCELABLE_SCHEDULATION_DETAILS);
 
@@ -61,4 +78,42 @@ public class DetailsScheduleActivity extends AppCompatActivity {
 
         return resultString;
     }
+
+    public void popUpdateConfirmationMessage(View view) throws IOException {
+        ScheduleValidatorForm validatorForm= new ScheduleValidatorForm(DetailsScheduleActivity.this,DetailsScheduleActivity.this,contacts);
+        boolean resultValidation = validatorForm.validate();
+        if(resultValidation) {
+            updateFragment = new UpdateScheduleConfirmationDialogFragment(this);
+            updateFragment.show(getSupportFragmentManager(),"updateFragment");
+        }
+
+    }
+    public void popRemoveConfirmationMessage(View view) throws IOException {
+
+        removeFragment = new RemoveScheduleConfirmationDialogFragment(this);
+        removeFragment.show(getSupportFragmentManager(),"removeFragment");
+
+    }
+
+    private void updateSchedulation() throws IOException {
+        //INTRODURRE LA LOGICA DI MODIFICA DELLA SCHEDULAZIONE
+    }
+
+    private void removeSchedulation() {
+        //INTRODURRE LA LOGICA DI RIMOZIONE DELLA SCHEDULAZIONE
+        //NOTA: nella rimozione ci vuole l'id alla schedulazione
+    }
+
+
+    @Override
+    public void onConfirmationUpdateSchedule() throws IOException {
+        updateSchedulation();
+    }
+
+    @Override
+    public void onConfirmationRemoveSchedule() throws IOException {
+        removeSchedulation();
+    }
+
+
 }
--- app/src/main/java/it/bancon/wascheduler/view/RemoveScheduleConfirmationDialogFragment.java
+++ app/src/main/java/it/bancon/wascheduler/view/RemoveScheduleConfirmationDialogFragment.java
@@ -1,65 +1,73 @@
 package it.bancon.wascheduler.view;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import it.bancon.wascheduler.R;
+import it.bancon.wascheduler.activity.MainAppActivity;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RemoveScheduleConfirmationDialogFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class RemoveScheduleConfirmationDialogFragment extends Fragment {
+import java.io.IOException;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class RemoveScheduleConfirmationDialogFragment extends DialogFragment {
+    private Context context;
+    private Button confirmRemoveButton;
+    private Button denyRemoveButton;
+    private RemoveScheduleListener listener;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public RemoveScheduleConfirmationDialogFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RemoveScheduleConfirmationDialogFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RemoveScheduleConfirmationDialogFragment newInstance(String param1, String param2) {
-        RemoveScheduleConfirmationDialogFragment fragment = new RemoveScheduleConfirmationDialogFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public RemoveScheduleConfirmationDialogFragment(Context context) {
+        this.context = context;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_remove_schedule_confirmation_dialog, container, false);
+
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getDialog().setCanceledOnTouchOutside(false);
+
+        confirmRemoveButton = getDialog().findViewById(R.id.buttonYesRemove);
+        denyRemoveButton = getDialog().findViewById(R.id.buttonNoRemove);
+
+        confirmRemoveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    listener.onConfirmationRemoveSchedule();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Intent intent = new Intent(context, MainAppActivity.class);
+                startActivity(intent);
+                dismiss();
+            }
+        });
+
+        denyRemoveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, MainAppActivity.class);
+                startActivity(intent);
+                dismiss();
+            }
+        });
+    }
+
+    public interface RemoveScheduleListener {
+        void onConfirmationRemoveSchedule() throws IOException;
+    }
+
 }
